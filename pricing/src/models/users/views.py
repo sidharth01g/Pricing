39952b31448e0af6946d87b468120a5c2615fd1f
sidharth01g <sidharth01g@gmail.com>
2018-06-04 20:02:21 +0530

    Create placeholder blueprints

---
+++ pricing/src/models/users/views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint
+
+user_blueprint = Blueprint(name='users', import_name=__name__)
+
+
+@user_blueprint.route('/login')
+def login_user():
+    pass
+
+
+@user_blueprint.route('/register')
+def register_user():
+    pass
+
+
+@user_blueprint.route('/alerts')
+def user_alerts():
+    pass
+
+
+@user_blueprint.route('/logout')
+def logout_user():
+    pass
