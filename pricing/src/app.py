965c8b94330b0652c5e76097913f2c807c861dd5
sidharth01g <sidharth01g@gmail.com>
2018-06-09 17:51:34 +0530

    Load config from yamle file

--- pricing/src/app.py
+++ pricing/src/app.py
@@ -1,8 +1,16 @@
 from flask import Flask
-
 from pricing.src.common.logging_base import Logging
+import os
+from pathlib2 import Path
+from pricing.src.common.config_loader import Config
 
 logger = Logging.create_rotating_log(module_name=__name__, logging_directory='/tmp')
+
+# Load config
+directory = Path(os.path.dirname(os.path.realpath(__name__)))
+config_filepath = directory.parent / 'config' / 'config.yaml'
+logger.debug('Config file: {}'. format(config_filepath))
+configuration = Config.load_config_yaml(config_filepath=config_filepath)
 
 app = Flask(__name__)
 
