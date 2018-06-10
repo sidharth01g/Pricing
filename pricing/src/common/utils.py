ecc20d564e0afb396200b452505f838c9ad54e20
sidharth01g <sidharth01g@gmail.com>
2018-06-10 23:37:33 +0530

    Add method to validate email IDs

--- pricing/src/common/utils.py
+++ pricing/src/common/utils.py
@@ -35,3 +35,21 @@ class Utils(object):
 
         return pbkdf2_sha512.verify(password_hashed, password_encrypted)
 
+    @staticmethod
+    def is_valid_email(email: str) -> bool:
+        """
+        Verifies that a set of conditions are satisfied in a given email string
+
+        Args:
+            email: Email ID
+
+        Returns:
+            True if all conditions for a valid email ID are satisfied, else False
+        """
+        conditions = [
+            '@' in email,
+            len(email.split('@')[0]) > 0,
+            len(email.split('@')[-1]) > 0,
+            '.' in email.split('@')[-1],
+        ]
+        return all(condition is True for condition in conditions)
