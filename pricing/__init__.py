85bb8d729bf2238b88e9725e13a1d78f6ef28502
sidharth01g <sidharth01g@gmail.com>
2018-06-10 23:11:01 +0530

    Move to single database instance in the pricing package's init method

--- pricing/__init__.py
+++ pricing/__init__.py
@@ -1,6 +1,7 @@
 from pathlib2 import Path
 from pricing.src.common.config_loader import Config
 from pricing.src.common.logging_base import Logging
+from pricing.src.common.database import Database
 import os
 
 logger = Logging.create_rotating_log(module_name=__name__, logging_directory='/tmp')
@@ -12,3 +13,7 @@ config_filepath = directory / 'config' / 'config.yaml'
 logger.debug('Config file: {}'. format(config_filepath))
 configuration = Config.load_config_yaml(config_filepath=config_filepath)
 print(configuration)
+
+db_name = configuration['database_name']
+uri = configuration['database_uri']
+db = Database(db_name=db_name, uri=uri)
