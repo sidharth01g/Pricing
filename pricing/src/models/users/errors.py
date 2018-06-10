86037b27f54400ce1eace786ed16b1e6b5a6c8a0
sidharth01g <sidharth01g@gmail.com>
2018-06-10 22:56:03 +0530

    Create base class for User exceptions

--- pricing/src/models/users/errors.py
+++ pricing/src/models/users/errors.py
@@ -3,21 +3,25 @@ from pricing.src.common.logging_base import Logging
 logger = Logging.create_rotating_log(module_name=__name__, logging_directory='/tmp')
 
 
-class UserNotExistsError(Exception):
-
+class UserError(Exception):
     def __init__(self, message: str) -> None:
         self.message = message
         logger.exception(self.__repr__())
+
+
+class UserNotExistsError(UserError):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
 
     def __repr__(self) -> str:
         return '<UserNotExistsError: {}>'.format(self.message)
 
 
-class IncorrectPasswordError(Exception):
+class IncorrectPasswordError(UserError):
 
     def __init__(self, message: str) -> None:
-        self.message = message
-        logger.exception(self.__repr__())
+        super().__init__(message)
 
     def __repr__(self) -> str:
         return '<IncorrectPasswordError: {}>'.format(self.message)
