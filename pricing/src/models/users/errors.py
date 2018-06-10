3125aab235209d50ab707917ec57a639263b52cb
sidharth01g <sidharth01g@gmail.com>
2018-06-10 19:34:25 +0530

    Add logging in user exceptions

--- pricing/src/models/users/errors.py
+++ pricing/src/models/users/errors.py
@@ -1,7 +1,13 @@
+from pricing.src.common.logging_base import Logging
+
+logger = Logging.create_rotating_log(module_name=__name__, logging_directory='/tmp')
+
+
 class UserNotExistsError(Exception):
 
     def __init__(self, message: str) -> None:
         self.message = message
+        logger.exception(self.__repr__())
 
     def __repr__(self) -> str:
         return '<UserNotExistsError: {}>'.format(self.message)
@@ -11,6 +17,7 @@ class IncorrectPasswordError(Exception):
 
     def __init__(self, message: str) -> None:
         self.message = message
+        logger.exception(self.__repr__())
 
     def __repr__(self) -> str:
         return '<IncorrectPasswordError: {}>'.format(self.message)
