d45745a8db0776993e11e09bb0b46a071415aaae
sidharth01g <sidharth01g@gmail.com>
2018-06-17 15:22:06 +0530

    Added blueprints

--- pricing/src/app.py
+++ pricing/src/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from pricing.src.common.logging_base import Logging
 import pricing
 
 # Import blueprints
 from pricing.src.models.users.views import user_blueprint
+from pricing.src.models.alerts.views import alert_blueprint
+from pricing.src.models.stores.views import store_blueprint
 
 logger = Logging.create_rotating_log(module_name=__name__, logging_directory=pricing.configuration['logging_directory'])
 
@@ -12,11 +14,13 @@ app.secret_key = 'abcd1234'
 
 # Register blueprints
 app.register_blueprint(blueprint=user_blueprint, url_prefix='/users')
+app.register_blueprint(blueprint=alert_blueprint, url_prefix='/alerts')
+app.register_blueprint(blueprint=store_blueprint, url_prefix='/stores')
 
 
 @app.route('/')
 def home():
-    return 'Welcome!'
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
