02042701eccaa872d4750c4317e46c7c6d52f434
sidharth01g <sidharth01g@gmail.com>
2018-06-17 15:21:24 +0530

    Added index endpoint method

--- pricing/src/models/stores/views.py
+++ pricing/src/models/stores/views.py
@@ -1,11 +1,13 @@
 from flask import Blueprint
 
-
 store_blueprint = Blueprint(name='stores', import_name='__name__')
+
+
+@store_blueprint.route('/')
+def index():
+    return "This is the stores index page"
 
 
 @store_blueprint.route('/store/<string:name>')
 def store_page(name: str):
     pass
-
-
