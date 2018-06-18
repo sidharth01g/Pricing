0508e61bef7a9acc1999e9c7e8ea485dc8050682
sidharth01g <sidharth01g@gmail.com>
2018-06-18 22:57:50 +0530

    Added alerts endpoint

--- pricing/src/models/alerts/views.py
+++ pricing/src/models/alerts/views.py
@@ -9,7 +9,7 @@ def index():
     email = session['email']
     if email is not None:
         alerts = Alert.find_by_email(email=email)
-        return render_template('alerts/alert.html', alerts=alerts)
+        return render_template('alerts/alerts.html', alerts=alerts)
 
     return render_template('alerts/message.html')
 
@@ -24,11 +24,12 @@ def deactivate_alert(alert_id: str):
     pass
 
 
-@alert_blueprint.route('/alert/<string:alert_id>')
+@alert_blueprint.route('/<string:alert_id>')
 def get_alert_page(alert_id: str):
-    pass
+    alert = Alert.find_one_by_id(_id=alert_id)
+    return render_template('alerts/alert.html', alert=alert)
 
 
 @alert_blueprint.route('/alerts_for_user/<string:user_id>')
-def get_alets_for_user(user_id: str):
+def get_alerts_for_user(user_id: str):
     pass
