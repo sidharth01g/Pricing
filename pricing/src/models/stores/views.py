7eef0b548dc7efdaaaef4f0be5e72fd94c944362
sidharth01g <sidharth01g@gmail.com>
2018-07-08 00:21:50 +0530

    Added store ednpoints

--- pricing/src/models/stores/views.py
+++ pricing/src/models/stores/views.py
@@ -1,13 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+from pricing.src.models.stores.store import Store
 
 store_blueprint = Blueprint(name='stores', import_name='__name__')
 
 
-@store_blueprint.route('/')
+@store_blueprint.route('/', strict_slashes=False)
 def index():
-    return "This is the stores index page"
+    stores = Store.get_all_stores()
+    return render_template('stores/store_index.html', stores=stores)
 
 
-@store_blueprint.route('/store/<string:name>')
-def store_page(name: str):
-    pass
+@store_blueprint.route('/store/<string:store_id>', strict_slashes=False)
+def store_page(store_id: str):
+    store = Store.find_one_by_id(_id=store_id)
+    if not store:
+        return redirect(location=url_for(endpoint='.index'))
+    return render_template('stores/store.html', store=store)
+
+
+@store_blueprint.route('/create', methods=['GET', 'POST'], strict_slashes=False)
+def create_store():
+    if request.method == 'POST':
+        return 'create post'
+    return render_template('stores/create_store.html')
