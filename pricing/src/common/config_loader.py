e7df571f867d10ec686703bbc58c64b8740c93e8
sidharth01g <sidharth01g@gmail.com>
2018-07-08 13:54:43 +0530

    Make admins a frozenset while loading config

--- pricing/src/common/config_loader.py
+++ pricing/src/common/config_loader.py
@@ -8,4 +8,12 @@ class Config(object):
     @staticmethod
     def load_config_yaml(config_filepath: Union[Path, str]) -> Dict:
         with open(str(config_filepath), 'r') as infile:
-            return yaml.load(infile)
+            config = yaml.load(infile)
+            config = Config.freeze_admins(config)
+            return config
+
+    @staticmethod
+    def freeze_admins(config: Dict) -> Dict:
+        if 'admins' in config:
+            config['admins'] = frozenset(config['admins'])
+        return config
