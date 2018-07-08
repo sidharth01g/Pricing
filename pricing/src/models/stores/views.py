10e07e2e602998603335b6f8f08f5a28b040bb41
sidharth01g <sidharth01g@gmail.com>
2018-07-08 10:28:17 +0530

    Added endpoint to edit store

--- pricing/src/models/stores/views.py
+++ pricing/src/models/stores/views.py
@@ -2,18 +2,19 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from pricing.src.models.stores.store import Store
 import pricing.src.models.stores.errors as store_errors
 import json
+import ast
 
 store_blueprint = Blueprint(name='stores', import_name='__name__')
 
 
 @store_blueprint.route('/', strict_slashes=False)
-def index():
+def index() -> str:
     stores = Store.get_all_stores()
     return render_template('stores/store_index.html', stores=stores)
 
 
 @store_blueprint.route('/store/<string:store_id>', strict_slashes=False)
-def store_page(store_id: str):
+def store_page(store_id: str) -> str:
     try:
         store = Store.find_one_by_id(_id=store_id)
     except store_errors.StoreNotFoundError:
@@ -22,7 +23,7 @@ def store_page(store_id: str):
 
 
 @store_blueprint.route('/create', methods=['GET', 'POST'], strict_slashes=False)
-def create_store():
+def create_store() -> str:
     if request.method == 'POST':
         store_name = request.form['name']
         url_prefix = request.form['url_prefix']
@@ -36,14 +37,30 @@ def create_store():
 
 
 @store_blueprint.route('/delete/<string:store_id>', methods=['GET', 'POST'], strict_slashes=False)
-def delete_store():
+def delete_store(store_id: str) -> str:
     if request.method == 'POST':
-        return 'Delete store'
+        return 'Delete store {}'.format(store_id)
     return 'Enter store ID'
 
 
 @store_blueprint.route('/edit/<string:store_id>', methods=['GET', 'POST'], strict_slashes=False)
-def edit_store():
+def edit_store(store_id: str) -> str:
+    try:
+        store = Store.find_one_by_id(_id=store_id)
+    except store_errors.StoreNotFoundError:
+        return redirect(location=url_for(endpoint='.index'))
+
     if request.method == 'POST':
-        return 'Edit post'
-    return 'Enter store ID'
+        store_name = request.form['name']
+        url_prefix = request.form['url_prefix']
+        tag_name = request.form['tag_name']
+        query = ast.literal_eval(request.form['query'])
+        assert type(query) is dict
+
+        store.name = store_name
+        store.url_prefix = url_prefix
+        store.tag_name = tag_name
+        store.query = query
+        store.update_in_database(upsert=False)
+        return redirect(location=url_for(endpoint='.index'))
+    return render_template('stores/edit_store.html', store=store)
