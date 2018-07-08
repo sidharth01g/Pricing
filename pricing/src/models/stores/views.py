8b2dd900e1ff5eec0763d12960c3f525a0e5b511
sidharth01g <sidharth01g@gmail.com>
2018-07-08 09:40:22 +0530

    Added store creation

--- pricing/src/models/stores/views.py
+++ pricing/src/models/stores/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from pricing.src.models.stores.store import Store
 import pricing.src.models.stores.errors as store_errors
+import json
 
 store_blueprint = Blueprint(name='stores', import_name='__name__')
 
@@ -23,14 +24,21 @@ def store_page(store_id: str):
 @store_blueprint.route('/create', methods=['GET', 'POST'], strict_slashes=False)
 def create_store():
     if request.method == 'POST':
-        return 'create post'
+        store_name = request.form['name']
+        url_prefix = request.form['url_prefix']
+        tag_name = request.form['tag_name']
+        query = json.loads(request.form['query'])
+
+        store = Store(name=store_name, url_prefix=url_prefix, tag_name=tag_name, query=query)
+        store.insert_into_database()
+        return redirect(location=url_for(endpoint='.index'))
     return render_template('stores/create_store.html')
 
 
 @store_blueprint.route('/delete/<string:store_id>', methods=['GET', 'POST'], strict_slashes=False)
 def delete_store():
     if request.method == 'POST':
-        return 'Delete post'
+        return 'Delete store'
     return 'Enter store ID'
 
 
